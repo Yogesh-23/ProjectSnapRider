7fbe6242f5be20697fe8dbfe850cbf802915d177
Yogesh-23 <yogeshpotdar23@gmail.com>
2019-02-24 15:24:48 +0530

    folder added

---
+++ SnapRider/src/main/java/com/alighthub/snapRider/SnapRiderApplication.java
@@ -0,0 +1,13 @@
+package com.alighthub.snapRider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnapRiderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnapRiderApplication.class, args);
+	}
+
+}
