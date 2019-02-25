de655c273c2fed9205b565f8ae44ba1bced8e669
poonamavhad <poonam.avhad94@gmail.com>
2019-02-25 15:31:14 +0530

    Cust Pojo

--- SnapRider/src/main/java/com/alighthub/snapRider/model/Customer.java
+++ SnapRider/src/main/java/com/alighthub/snapRider/model/Customer.java
@@ -1,7 +1,12 @@
 package com.alighthub.snapRider.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Customer {
 
+	@Id
 	private int customerId;
 	private String customerName;
 	private String customerAddress;
