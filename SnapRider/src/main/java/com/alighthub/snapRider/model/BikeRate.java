75c4373f0904026bf17a80414f031a30e9f9afe7
madhukar <saknuremadhu2747@gmail.com>
2019-02-25 14:41:35 +0530

    push

--- SnapRider/src/main/java/com/alighthub/snapRider/model/BikeRate.java
+++ SnapRider/src/main/java/com/alighthub/snapRider/model/BikeRate.java
@@ -1,7 +1,32 @@
 package com.alighthub.snapRider.model;
 
+import javax.persistence.Entity;
+
+@Entity
 public class BikeRate {
 
-	//bike Rider  changes
+	private int perHours;
+	private int perDay;
+	private int perWeek;
+	
+	public int getPerHours() {
+		return perHours;
+	}
+	public void setPerHours(int perHours) {
+		this.perHours = perHours;
+	}
+	public int getPerDay() {
+		return perDay;
+	}
+	public void setPerDay(int perDay) {
+		this.perDay = perDay;
+	}
+	public int getPerWeek() {
+		return perWeek;
+	}
+	public void setPerWeek(int perWeek) {
+		this.perWeek = perWeek;
+	}
+	
 	
 }
--- SnapRider/src/main/java/com/alighthub/snapRider/model/Customer.java
+++ SnapRider/src/main/java/com/alighthub/snapRider/model/Customer.java
@@ -1,5 +1,66 @@
 package com.alighthub.snapRider.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Customer {
 
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private int customerId;
+	private String customerFirstName;
+	private String customerLastName;
+	private String customerLicenseNumber;
+	private String customerMobileNumber;
+	private String customerEmail;
+	private String customerPassword;
+	
+	public int getCustomerId() {
+		return customerId;
+	}
+	public void setCustomerId(int customerId) {
+		this.customerId = customerId;
+	}
+	public String getCustomerFirstName() {
+		return customerFirstName;
+	}
+	public void setCustomerFirstName(String customerFirstName) {
+		this.customerFirstName = customerFirstName;
+	}
+	public String getCustomerLastName() {
+		return customerLastName;
+	}
+	public void setCustomerLastName(String customerLastName) {
+		this.customerLastName = customerLastName;
+	}
+	public String getCustomerLicenseNumber() {
+		return customerLicenseNumber;
+	}
+	public void setCustomerLicenseNumber(String customerLicenseNumber) {
+		this.customerLicenseNumber = customerLicenseNumber;
+	}
+	public String getCustomerMobileNumber() {
+		return customerMobileNumber;
+	}
+	public void setCustomerMobileNumber(String customerMobileNumber) {
+		this.customerMobileNumber = customerMobileNumber;
+	}
+	public String getCustomerEmail() {
+		return customerEmail;
+	}
+	public void setCustomerEmail(String customerEmail) {
+		this.customerEmail = customerEmail;
+	}
+	public String getCustomerPassword() {
+		return customerPassword;
+	}
+	public void setCustomerPassword(String customerPassword) {
+		this.customerPassword = customerPassword;
+	}
+	
+	
+	
 }
