19617974d0626a637174135f0e9e80c11499831a
poonamavhad <poonam.avhad94@gmail.com>
2019-02-25 15:17:09 +0530

    customer model

---
+++ SnapRider/src/main/java/com/alighthub/snapRider/model/Customer.java
@@ -0,0 +1,56 @@
+package com.alighthub.snapRider.model;
+
+public class Customer {
+
+	private int customerId;
+	private String customerFirstName;
+	private String customerAddress;
+	private String customerEmail;
+	private String customerMobileNumber;
+	private String customerPassword;
+	
+	
+	
+	
+	public int getCustomerId() {
+		return customerId;
+	}
+	public void setCustomerId(int customerId) {
+		this.customerId = customerId;
+	}
+	public String getCustomerFirstName() {
+		return customerFirstName;
+	}
+	public void setCustomerFirstName(String customerFirstName) {
+		this.customerFirstName = customerFirstName;
+	}
+	public String getCustomerAddress() {
+		return customerAddress;
+	}
+	public void setCustomerAddress(String customerAddress) {
+		this.customerAddress = customerAddress;
+	}
+	public String getCustomerEmail() {
+		return customerEmail;
+	}
+	public void setCustomerEmail(String customerEmail) {
+		this.customerEmail = customerEmail;
+	}
+	public String getCustomerMobileNumber() {
+		return customerMobileNumber;
+	}
+	public void setCustomerMobileNumber(String customerMobileNumber) {
+		this.customerMobileNumber = customerMobileNumber;
+	}
+	public String getCustomerPassword() {
+		return customerPassword;
+	}
+	public void setCustomerPassword(String customerPassword) {
+		this.customerPassword = customerPassword;
+	}
+
+	
+	
+	
+	
+}
